04a18bb531c57420fc8543873a72219489060526
feyton <tumbafabruce@gmail.com>
2020-05-06 16:49:22 +0200

    Branching and admin

---
+++ script/store.js
@@ -0,0 +1,19 @@
+
+// $(document).ready(function () {
+//     $('.product-wrapper').slick({
+//         rtl: true,
+//         autoplay: true,
+//         autoplaySpeed: 1000,
+//     });
+//     $("#input_quantity").change(function () {
+//         console.log($(this).val())
+//     })
+// })
+
+
+
+
+
+
+
+
